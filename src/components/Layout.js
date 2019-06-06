548788fac7f463656ca6626f269296a85a162780
Jakub Sarnowski <sarneeh@gmail.com>
2019-06-07 00:02:05 +0200

    feat: responsive footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,9 +5,21 @@ export default ({ children }) => (
     <main className="text-xs lg:text-base relative overflow-hidden">
       {children}
     </main>
-    <footer className="flex justify-between bg-main py-2 px-4 text-xs text-white">
-      <span>Copyright © 2019 Jakub Sarnowski</span>
-      <a href="https://www.gatsbyjs.org">Powered by GatsbyJS</a>
+    <footer
+      className="flex flex-wrap justify-center sm:justify-between bg-main py-2 px-4 text-white"
+      style={{
+        fontSize: 16
+      }}
+    >
+      <span className="w-full sm:w-auto text-center">
+        Copyright © 2019 Jakub Sarnowski
+      </span>
+      <a
+        className="w-full sm:w-auto text-center"
+        href="https://www.gatsbyjs.org"
+      >
+        Powered by GatsbyJS
+      </a>
     </footer>
   </React.Fragment>
 );
